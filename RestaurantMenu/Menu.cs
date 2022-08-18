cd36cba5459179e79dba83c7b66c4866a48e1553
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-17 19:59:45 -0500

    completed studio

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+       
+    }
+}
+
---
+++ RestaurantMenu/MenuItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RestaurantMenu
+{
+    public class MenuItems
+    {
+        private string _name;
+        public double _price;
+        private string _description;
+        private string _category;
+        public bool isNew;
+
+    }
+}
+
